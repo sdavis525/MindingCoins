7effbae2ae8d84fef988b6c27bbf7a930198640d
Sophia Davis <sophiadavis@Sophias-MBP.attlocal.net>
2022-06-21 10:14:28 -0400

    fixed mongodb connection

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/budget";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/budget-tracker-db', {
+mongoose.connect(process.env.MONGODB_URI
 
-});
+);
 
 // routes
 app.use(require("./routes/api.js"));
